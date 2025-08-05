3bd114133617eaa6022bfe6b8bf336c2c89797ac
2chainzMc <thulaning62@gmail.com>
2025-08-05 02:55:18 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/parent-pulse-platform/" : "/", // 👈 Important for GitHub Pages
+  base: mode === "production" ? "/parent-pulse-platform/" : "/", 
   server: {
     host: "::",
     port: 8080,
